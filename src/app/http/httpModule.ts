eae9dc8f673d92ce074717da01934e2150f99281
michalovsky <michalovskyyy@gmail.com>
2021-11-20 23:55:34 +0100

    feat: add tag module

--- src/app/http/httpModule.ts
+++ src/app/http/httpModule.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from './auth/authModule';
 import { httpConfigProvider } from './providers/httpConfig';
+import { TagModule } from './tag/tagModule';
 import { UserModule } from './user/userModule';
 import { UserResourceModule } from './userResource/userResourceModule';
 
 @Module({
-  imports: [UserModule, AuthModule, UserResourceModule],
+  imports: [UserModule, AuthModule, UserResourceModule, TagModule],
   providers: [httpConfigProvider],
-  exports: [httpConfigProvider, UserModule, AuthModule, UserResourceModule],
+  exports: [httpConfigProvider, UserModule, AuthModule, UserResourceModule, TagModule],
 })
 export class HttpModule {}
