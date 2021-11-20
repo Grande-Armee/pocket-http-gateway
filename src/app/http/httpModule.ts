bd9112a3901f0d6614d0c839cde8723a7a1e41da
michalovsky <michalovskyyy@gmail.com>
2021-11-20 20:00:53 +0100

    feat: add user resource module

--- src/app/http/httpModule.ts
+++ src/app/http/httpModule.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/authModule';
 import { httpConfigProvider } from './providers/httpConfig';
 import { UserModule } from './user/userModule';
+import { UserResourceModule } from './userResource/userResourceModule';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, UserResourceModule],
   providers: [httpConfigProvider],
-  exports: [httpConfigProvider, UserModule, AuthModule],
+  exports: [httpConfigProvider, UserModule, AuthModule, UserResourceModule],
 })
 export class HttpModule {}
