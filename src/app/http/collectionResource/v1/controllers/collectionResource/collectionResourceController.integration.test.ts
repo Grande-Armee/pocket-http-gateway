6206bb53335bf9a9c03f2066bdb79257bbb15468
michalovsky <michalovskyyy@gmail.com>
2021-11-25 17:16:21 +0100

    feat: add collection resource controller

---
+++ src/app/http/collectionResource/v1/controllers/collectionResource/collectionResourceController.integration.test.ts
@@ -0,0 +1,236 @@
+import { HttpStatus, INestApplication } from '@nestjs/common';
+
+import { TestModuleHelper } from '@integration/helpers/testModuleHelper/testModuleHelper';
+import { AuthService } from '@src/app/http/auth/services/auth/authService';
+import { AuthHelper } from '@src/integration/helpers/authHelper/authHelper';
+import { HttpHelper, HttpMethod } from '@src/integration/helpers/httpHelper/httpHelper';
+
+class AuthServiceFake {
+  public async verifyToken(): Promise<void> {}
+}
+
+const baseUrl = '/v1/collections';
+const userIdField = 'userId';
+
+describe(`CollectionResourceV1Controller (${baseUrl})`, () => {
+  let app: INestApplication;
+  let httpHelper: HttpHelper;
+  let authHelper: AuthHelper;
+
+  beforeEach(async () => {
+    const moduleRef = await TestModuleHelper.createTestingModule()
+      .overrideProvider(AuthService)
+      .useClass(AuthServiceFake)
+      .compile();
+
+    app = await TestModuleHelper.createApp(moduleRef);
+    httpHelper = new HttpHelper(app);
+    authHelper = new AuthHelper(app);
+  });
+
+  afterEach(async () => {
+    await app.close();
+  });
+
+  describe('Create collection resource', () => {
+    it('throws an error when the userId query is not a uuid', async () => {
+      expect.assertions(1);
+
+      const userId = '123';
+      const resourceId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const collectionId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+
+      const authToken = authHelper.mockAuth({
+        userId,
+        role: '123',
+      });
+
+      const response = await httpHelper.request({
+        method: HttpMethod.POST,
+        url: `${baseUrl}/${collectionId}/resources/${resourceId}?${userIdField}=${userId}`,
+        token: authToken,
+      });
+
+      expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+    });
+
+    it('throws an error when the resourceId param is not uuid', async () => {
+      expect.assertions(1);
+
+      const userId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const resourceId = '123';
+      const collectionId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+
+      const authToken = authHelper.mockAuth({
+        userId,
+        role: '123',
+      });
+
+      const response = await httpHelper.request({
+        method: HttpMethod.POST,
+        url: `${baseUrl}/${collectionId}/resources/${resourceId}?${userIdField}=${userId}`,
+        token: authToken,
+      });
+
+      expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+    });
+
+    it('throws an error when the collectionId patam is not uuid', async () => {
+      expect.assertions(1);
+
+      const userId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const resourceId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const collectionId = '123';
+
+      const authToken = authHelper.mockAuth({
+        userId,
+        role: '123',
+      });
+
+      const response = await httpHelper.request({
+        method: HttpMethod.POST,
+        url: `${baseUrl}/${collectionId}/resources/${resourceId}?${userIdField}=${userId}`,
+        token: authToken,
+      });
+
+      expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+    });
+
+    it('accepts a request when the userId, resourceId and collectionId are uuid', async () => {
+      expect.assertions(1);
+
+      const userId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const resourceId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const collectionId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+
+      const authToken = authHelper.mockAuth({
+        userId,
+        role: '123',
+      });
+
+      const response = await httpHelper.request({
+        method: HttpMethod.POST,
+        url: `${baseUrl}/${collectionId}/resources/${resourceId}?${userIdField}=${userId}`,
+        token: authToken,
+      });
+
+      expect(response.statusCode).toBe(HttpStatus.CREATED);
+    });
+
+    it('requires bearer token authentication', async () => {
+      expect.assertions(1);
+
+      const userId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const resourceId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const collectionId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+
+      const response = await httpHelper.request({
+        method: HttpMethod.POST,
+        url: `${baseUrl}/${collectionId}/resources/${resourceId}?${userIdField}=${userId}`,
+      });
+
+      expect(response.statusCode).toBe(HttpStatus.FORBIDDEN);
+    });
+  });
+
+  describe('Remove collection resource', () => {
+    it('throws an error when the userId query is not a uuid', async () => {
+      expect.assertions(1);
+
+      const userId = '123';
+      const resourceId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const collectionId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+
+      const authToken = authHelper.mockAuth({
+        userId,
+        role: '123',
+      });
+
+      const response = await httpHelper.request({
+        method: HttpMethod.DELETE,
+        url: `${baseUrl}/${collectionId}/resources/${resourceId}?${userIdField}=${userId}`,
+        token: authToken,
+      });
+
+      expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+    });
+
+    it('throws an error when the resourceId param is not uuid', async () => {
+      expect.assertions(1);
+
+      const userId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const resourceId = '123';
+      const collectionId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+
+      const authToken = authHelper.mockAuth({
+        userId,
+        role: '123',
+      });
+
+      const response = await httpHelper.request({
+        method: HttpMethod.DELETE,
+        url: `${baseUrl}/${collectionId}/resources/${resourceId}?${userIdField}=${userId}`,
+        token: authToken,
+      });
+
+      expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+    });
+
+    it('throws an error when the collectionId patam is not uuid', async () => {
+      expect.assertions(1);
+
+      const userId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const resourceId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const collectionId = '123';
+
+      const authToken = authHelper.mockAuth({
+        userId,
+        role: '123',
+      });
+
+      const response = await httpHelper.request({
+        method: HttpMethod.DELETE,
+        url: `${baseUrl}/${collectionId}/resources/${resourceId}?${userIdField}=${userId}`,
+        token: authToken,
+      });
+
+      expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+    });
+
+    it('accepts a request when the userId, resourceId and collectionId are uuid', async () => {
+      expect.assertions(1);
+
+      const userId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const resourceId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const collectionId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+
+      const authToken = authHelper.mockAuth({
+        userId,
+        role: '123',
+      });
+
+      const response = await httpHelper.request({
+        method: HttpMethod.DELETE,
+        url: `${baseUrl}/${collectionId}/resources/${resourceId}?${userIdField}=${userId}`,
+        token: authToken,
+      });
+
+      expect(response.statusCode).toBe(HttpStatus.NO_CONTENT);
+    });
+
+    it('requires bearer token authentication', async () => {
+      expect.assertions(1);
+
+      const userId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const resourceId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+      const collectionId = 'e46c11a8-8893-412d-bc8b-60753a98e45c';
+
+      const response = await httpHelper.request({
+        method: HttpMethod.DELETE,
+        url: `${baseUrl}/${collectionId}/resources/${resourceId}?${userIdField}=${userId}`,
+      });
+
+      expect(response.statusCode).toBe(HttpStatus.FORBIDDEN);
+    });
+  });
+});
---
+++ src/app/http/collectionResource/v1/controllers/collectionResource/collectionResourceController.ts
@@ -0,0 +1,120 @@
+import { DtoFactory } from '@grande-armee/pocket-common';
+import {
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+  ForbiddenException,
+} from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+
+import { BearerTokenPayload } from '@http/auth/decorators/authTokenPayload/authTokenPayloadDecorator';
+import { BearerTokenGuard } from '@http/auth/guards/bearerToken/bearerTokenGuard';
+import { AuthPayload } from '@http/auth/interfaces';
+
+import {
+  CreateCollectionResourceParamsV1Dto,
+  CreateCollectionResourceResponseV1Dto,
+  CreateCollectionResourceQueryV1Dto,
+} from '../../dtos/createCollectionResourceDto';
+import {
+  RemoveCollectionResourceParamsV1Dto,
+  RemoveCollectionResourceQueryV1Dto,
+} from '../../dtos/removeCollectionResourceDto';
+import { CollectionResourceV1Service } from '../../services/collectionResource/collectionResourceService';
+
+@ApiTags('CollectionResources')
+@Controller({ version: '1', path: '/collections/:collectionId/resources/:resourceId' })
+export class CollectionResourceV1Controller {
+  public constructor(
+    private readonly collectionResourceService: CollectionResourceV1Service,
+    private readonly dtoFactory: DtoFactory,
+  ) {}
+
+  @ApiOperation({
+    description: 'Create collection resource.',
+  })
+  @ApiCreatedResponse({
+    description: 'Collection resource created.',
+    type: CreateCollectionResourceResponseV1Dto,
+  })
+  @UseGuards(BearerTokenGuard)
+  @HttpCode(HttpStatus.CREATED)
+  @Post()
+  public async createCollectionResource(
+    @Param() createCollectionResourceParams: CreateCollectionResourceParamsV1Dto,
+    @Query() createCollectionResourceQuery: CreateCollectionResourceQueryV1Dto,
+    @BearerTokenPayload() authPayload: AuthPayload,
+  ): Promise<CreateCollectionResourceResponseV1Dto> {
+    const { userId } = createCollectionResourceQuery;
+
+    if (authPayload.userId !== userId) {
+      throw new ForbiddenException('User id from auth token does not match user id from query.');
+    }
+
+    const { resourceId, collectionId } = createCollectionResourceParams;
+
+    const result = await this.collectionResourceService.createCollectionResource({
+      userId,
+      resourceId,
+      collectionId,
+    });
+
+    console.log(result);
+
+    return this.dtoFactory.createDtoInstance(CreateCollectionResourceResponseV1Dto, {
+      collectionResource: {
+        id: '123',
+        createdAt: '123',
+        updatedAt: '123',
+        userId: '123',
+        resourceId: '123',
+        collectionId: '123',
+      },
+    });
+  }
+
+  @ApiBearerAuth()
+  @ApiOperation({
+    description: 'Remove collection resource by id.',
+  })
+  @ApiNoContentResponse({
+    description: 'Collection resource removed.',
+  })
+  @ApiForbiddenResponse()
+  @ApiNotFoundResponse()
+  @UseGuards(BearerTokenGuard)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete()
+  public async removeCollectionResource(
+    @Param() removeCollectionResourceParams: RemoveCollectionResourceParamsV1Dto,
+    @Query() removeCollectionResourceQuery: RemoveCollectionResourceQueryV1Dto,
+    @BearerTokenPayload() authPayload: AuthPayload,
+  ): Promise<void> {
+    const { userId } = removeCollectionResourceQuery;
+
+    if (authPayload.userId !== userId) {
+      throw new ForbiddenException('User id from auth token does not match user id from query.');
+    }
+
+    const { resourceId, collectionId } = removeCollectionResourceParams;
+
+    await this.collectionResourceService.removeCollectionResource({
+      userId,
+      resourceId,
+      collectionId,
+    });
+  }
+}
