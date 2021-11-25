e4df3b73ad7d6f40b660be1f73de502f17e68ba2
michalovsky <michalovskyyy@gmail.com>
2021-11-25 17:00:05 +0100

    feat: add collection module

--- src/app/http/httpModule.ts
+++ src/app/http/httpModule.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from './auth/authModule';
+import { CollectionModule } from './collection/collectionModule';
 import { httpConfigProvider } from './providers/httpConfig';
 import { TagModule } from './tag/tagModule';
 import { UserModule } from './user/userModule';
@@ -8,8 +9,16 @@ import { UserResourceModule } from './userResource/userResourceModule';
 import { UserResourceTagModule } from './userResourceTag/userResourceTagModule';
 
 @Module({
-  imports: [UserModule, AuthModule, UserResourceModule, TagModule, UserResourceTagModule],
+  imports: [UserModule, AuthModule, UserResourceModule, TagModule, UserResourceTagModule, CollectionModule],
   providers: [httpConfigProvider],
-  exports: [httpConfigProvider, UserModule, AuthModule, UserResourceModule, TagModule, UserResourceTagModule],
+  exports: [
+    httpConfigProvider,
+    UserModule,
+    AuthModule,
+    UserResourceModule,
+    TagModule,
+    UserResourceTagModule,
+    CollectionModule,
+  ],
 })
 export class HttpModule {}
